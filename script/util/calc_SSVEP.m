acf6615d0cb3e0647911cf8b4a171b15a2aaac1b
Rakesh Bute <rakeshbute@gmail.com>
2015-11-26 17:40:27 +0530

    Analysis commit

--- script/util/calc_SSVEP.m
+++ script/util/calc_SSVEP.m
@@ -5,13 +5,19 @@ function [ output_args,plotted_freqs ] = calc_SSVEP( input_args )
 global f1
 global f2
 global gBlockSecsPerTick;
-global calculationMatrix;
+% global calculationMatrix;
+
+calculationMatrix = ...
+    [  -1 -1 -1 -1 4];
 
 analysisTimeFFTPoints = 5000;
 freqs = (1/(gBlockSecsPerTick*analysisTimeFFTPoints))...
     *(0:(analysisTimeFFTPoints-1));
 
-output_args= abs(fft((calculationMatrix * input_args)',analysisTimeFFTPoints,1));
+[result]=pca(input_args);
+
+output_args= abs([fft(result,analysisTimeFFTPoints,1),...
+    fft((result*calculationMatrix'),analysisTimeFFTPoints,1)]);
 
 plotted_freqs=freqs(find(freqs>=f1 & freqs <=f2))';
 output_args=output_args(find(freqs>=f1 & freqs <=f2),:);
